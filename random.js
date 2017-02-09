992bc5dc992fa1c2ad849a4ab77242434a10533a
Fon <aaron.s.fn@gmail.com>
2017-02-09 12:54:54 -0800

    aafon eatsz
    haaa

--- random.js
+++ random.js
@@ -5,7 +5,91 @@ var namespace = "http://www.w3.org/2000/svg"
 // You need to use at least 3 different types of shape.
 // Remember, for the shapes to show up on the canvas, you'll need to CALL the function.
 function createFirstScene() {
-    
+
+//THE DEATHSTAR//
+makeCircle(25,25,10,"dimgray",1)
+makeRect(15,25,10,0.25,"black",0.5)
+makeRect(25,25,10,0.25,"black",0.5)
+makeEllipse(20,20,2.5,2.5,"gray",1)
+
+
+//THE STARS//
+makeCircle(1,1,.1,"white",1)
+makeCircle(3,12,.1,"white",1)
+makeCircle(2,2,.1,"white",1)
+makeCircle(5,6,.1,"white",1)
+makeCircle(4,19,.1,"white",1)
+makeCircle(9,14,.1,"white",1)
+makeCircle(12,10,.1,"white",1)
+makeCircle(17,2,.1,"white",1)
+makeCircle(20,1,.1,"white",1)
+makeCircle(1,25,.1,"white",1)
+makeCircle(25,10,.1,"white",1)
+makeCircle(31,1,.1,"white",1)
+makeCircle(21,12,.1,"white",1)
+makeCircle(30,11,.1,"white",1)
+makeCircle(32,45,.1,"white",1)
+makeCircle(22,5,.1,"white",1)
+makeCircle(31,1,.1,"white",1)
+makeCircle(22,2,.1,"white",1)
+makeCircle(21,15,.1,"white",1)
+makeCircle(24,1,.1,"white",1)
+makeCircle(35,12,.1,"white",1)
+makeCircle(12,35,.1,"white",1)
+makeCircle(21,1,.1,"white",1)
+makeCircle(22,10,.1,"white",1)
+makeCircle(5,1,.1,"white",1)
+makeCircle(9,1,.1,"white",1)
+makeCircle(45,31,.1,"white",1)
+makeCircle(27,13,.1,"white",1)
+makeCircle(40,2,.1,"white",1)
+makeCircle(43,3,.1,"white",1)
+makeCircle(50,1,.1,"white",1)
+makeCircle(49,21,.1,"white",1)
+makeCircle(45,12,.1,"white",1)
+makeCircle(45,1,.1,"white",1)
+makeCircle(46,9,.1,"white",1)
+makeCircle(37,11,.1,"white",1)
+makeCircle(33,2,.1,"white",1)
+makeCircle(23,45,.1,"white",1)
+makeCircle(1,43,.1,"white",1)
+makeCircle(2,40,.1,"white",1)
+makeCircle(4,34,.1,"white",1)
+makeCircle(5,38,.1,"white",1)
+makeCircle(23,11,.1,"white",1)
+makeCircle(21,49,.1,"white",1)
+makeCircle(19,34,.1,"white",1)
+makeCircle(21,40,.1,"white",1)
+makeCircle(31,38,.1,"white",1)
+makeCircle(32,37,.1,"white",1)
+makeCircle(39,42,.1,"white",1)
+makeCircle(41,38,.1,"white",1)
+makeCircle(40,41,.1,"white",1)
+makeCircle(45,45,.1,"white",1)
+makeCircle(49,48,.1,"white",1)
+makeCircle(50,34,.1,"white",1)
+makeCircle(25,41,.1,"white",1)
+makeCircle(25,48,.1,"white",1)
+makeCircle(1,31,.1,"white",1)
+makeCircle(3,41,.1,"white",1)
+makeCircle(6,39,.1,"white",1)
+makeCircle(9,48,.1,"white",1)
+makeCircle(14,32,.1,"white",1)
+makeCircle(13,45,.1,"white",1)
+makeCircle(41,48,.1,"white",1)
+makeCircle(35,34,.1,"white",1)
+makeCircle(5,32,.1,"white",1)
+makeCircle(7,21,.1,"white",1)
+makeCircle(9,22,.1,"white",1)
+makeCircle(11,24,.1,"white",1)
+makeCircle(13,34,.1,"white",1)
+makeCircle(17,46,.1,"white",1)
+makeCircle(46,32,.1,"white",1)
+makeCircle(47,42,.1,"white",1)
+makeCircle(42,23,.1,"white",1)
+makeCircle(40,21,.1,"white",1)
+makeCircle(35,12,.1,"white",1)
+makeCircle(3,47,.1,"white",1)
 }
 
 
@@ -14,6 +98,84 @@ function createFirstScene() {
 // Remember, for the shapes to show up on the canvas, you'll need to CALL the function.
 function createSecondScene() {
     
+//THE STARS//
+makeCircle(1,1,.1,"white",1)
+makeCircle(3,12,.1,"white",1)
+makeCircle(2,2,.1,"white",1)
+makeCircle(5,6,.1,"white",1)
+makeCircle(4,19,.1,"white",1)
+makeCircle(9,14,.1,"white",1)
+makeCircle(12,10,.1,"white",1)
+makeCircle(17,2,.1,"white",1)
+makeCircle(20,1,.1,"white",1)
+makeCircle(1,25,.1,"white",1)
+makeCircle(25,10,.1,"white",1)
+makeCircle(31,1,.1,"white",1)
+makeCircle(21,12,.1,"white",1)
+makeCircle(30,11,.1,"white",1)
+makeCircle(32,45,.1,"white",1)
+makeCircle(22,5,.1,"white",1)
+makeCircle(31,1,.1,"white",1)
+makeCircle(22,2,.1,"white",1)
+makeCircle(21,15,.1,"white",1)
+makeCircle(24,1,.1,"white",1)
+makeCircle(35,12,.1,"white",1)
+makeCircle(12,35,.1,"white",1)
+makeCircle(21,1,.1,"white",1)
+makeCircle(22,10,.1,"white",1)
+makeCircle(5,1,.1,"white",1)
+makeCircle(9,1,.1,"white",1)
+makeCircle(45,31,.1,"white",1)
+makeCircle(27,13,.1,"white",1)
+makeCircle(40,2,.1,"white",1)
+makeCircle(43,3,.1,"white",1)
+makeCircle(50,1,.1,"white",1)
+makeCircle(49,21,.1,"white",1)
+makeCircle(45,12,.1,"white",1)
+makeCircle(45,1,.1,"white",1)
+makeCircle(46,9,.1,"white",1)
+makeCircle(37,11,.1,"white",1)
+makeCircle(33,2,.1,"white",1)
+makeCircle(23,45,.1,"white",1)
+makeCircle(1,43,.1,"white",1)
+makeCircle(2,40,.1,"white",1)
+makeCircle(4,34,.1,"white",1)
+makeCircle(5,38,.1,"white",1)
+makeCircle(23,11,.1,"white",1)
+makeCircle(21,49,.1,"white",1)
+makeCircle(19,34,.1,"white",1)
+makeCircle(21,40,.1,"white",1)
+makeCircle(31,38,.1,"white",1)
+makeCircle(32,37,.1,"white",1)
+makeCircle(39,42,.1,"white",1)
+makeCircle(41,38,.1,"white",1)
+makeCircle(40,41,.1,"white",1)
+makeCircle(45,45,.1,"white",1)
+makeCircle(49,48,.1,"white",1)
+makeCircle(50,34,.1,"white",1)
+makeCircle(25,41,.1,"white",1)
+makeCircle(25,48,.1,"white",1)
+makeCircle(1,31,.1,"white",1)
+makeCircle(3,41,.1,"white",1)
+makeCircle(6,39,.1,"white",1)
+makeCircle(9,48,.1,"white",1)
+makeCircle(14,32,.1,"white",1)
+makeCircle(13,45,.1,"white",1)
+makeCircle(41,48,.1,"white",1)
+makeCircle(35,34,.1,"white",1)
+makeCircle(5,32,.1,"white",1)
+makeCircle(7,21,.1,"white",1)
+makeCircle(9,22,.1,"white",1)
+makeCircle(11,24,.1,"white",1)
+makeCircle(13,34,.1,"white",1)
+makeCircle(17,46,.1,"white",1)
+makeCircle(46,32,.1,"white",1)
+makeCircle(47,42,.1,"white",1)
+makeCircle(42,23,.1,"white",1)
+makeCircle(40,21,.1,"white",1)
+makeCircle(35,12,.1,"white",1)
+makeCircle(3,47,.1,"white",1)
+    
 }
 
 
@@ -21,7 +183,84 @@ function createSecondScene() {
 // You need to use at least 3 different types of shape.
 // Remember, for the shapes to show up on the canvas, you'll need to CALL the function.
 function createThirdScene() {
-    
+//THE STARS//
+makeCircle(1,1,.1,"white",1)
+makeCircle(3,12,.1,"white",1)
+makeCircle(2,2,.1,"white",1)
+makeCircle(5,6,.1,"white",1)
+makeCircle(4,19,.1,"white",1)
+makeCircle(9,14,.1,"white",1)
+makeCircle(12,10,.1,"white",1)
+makeCircle(17,2,.1,"white",1)
+makeCircle(20,1,.1,"white",1)
+makeCircle(1,25,.1,"white",1)
+makeCircle(25,10,.1,"white",1)
+makeCircle(31,1,.1,"white",1)
+makeCircle(21,12,.1,"white",1)
+makeCircle(30,11,.1,"white",1)
+makeCircle(32,45,.1,"white",1)
+makeCircle(22,5,.1,"white",1)
+makeCircle(31,1,.1,"white",1)
+makeCircle(22,2,.1,"white",1)
+makeCircle(21,15,.1,"white",1)
+makeCircle(24,1,.1,"white",1)
+makeCircle(35,12,.1,"white",1)
+makeCircle(12,35,.1,"white",1)
+makeCircle(21,1,.1,"white",1)
+makeCircle(22,10,.1,"white",1)
+makeCircle(5,1,.1,"white",1)
+makeCircle(9,1,.1,"white",1)
+makeCircle(45,31,.1,"white",1)
+makeCircle(27,13,.1,"white",1)
+makeCircle(40,2,.1,"white",1)
+makeCircle(43,3,.1,"white",1)
+makeCircle(50,1,.1,"white",1)
+makeCircle(49,21,.1,"white",1)
+makeCircle(45,12,.1,"white",1)
+makeCircle(45,1,.1,"white",1)
+makeCircle(46,9,.1,"white",1)
+makeCircle(37,11,.1,"white",1)
+makeCircle(33,2,.1,"white",1)
+makeCircle(23,45,.1,"white",1)
+makeCircle(1,43,.1,"white",1)
+makeCircle(2,40,.1,"white",1)
+makeCircle(4,34,.1,"white",1)
+makeCircle(5,38,.1,"white",1)
+makeCircle(23,11,.1,"white",1)
+makeCircle(21,49,.1,"white",1)
+makeCircle(19,34,.1,"white",1)
+makeCircle(21,40,.1,"white",1)
+makeCircle(31,38,.1,"white",1)
+makeCircle(32,37,.1,"white",1)
+makeCircle(39,42,.1,"white",1)
+makeCircle(41,38,.1,"white",1)
+makeCircle(40,41,.1,"white",1)
+makeCircle(45,45,.1,"white",1)
+makeCircle(49,48,.1,"white",1)
+makeCircle(50,34,.1,"white",1)
+makeCircle(25,41,.1,"white",1)
+makeCircle(25,48,.1,"white",1)
+makeCircle(1,31,.1,"white",1)
+makeCircle(3,41,.1,"white",1)
+makeCircle(6,39,.1,"white",1)
+makeCircle(9,48,.1,"white",1)
+makeCircle(14,32,.1,"white",1)
+makeCircle(13,45,.1,"white",1)
+makeCircle(41,48,.1,"white",1)
+makeCircle(35,34,.1,"white",1)
+makeCircle(5,32,.1,"white",1)
+makeCircle(7,21,.1,"white",1)
+makeCircle(9,22,.1,"white",1)
+makeCircle(11,24,.1,"white",1)
+makeCircle(13,34,.1,"white",1)
+makeCircle(17,46,.1,"white",1)
+makeCircle(46,32,.1,"white",1)
+makeCircle(47,42,.1,"white",1)
+makeCircle(42,23,.1,"white",1)
+makeCircle(40,21,.1,"white",1)
+makeCircle(35,12,.1,"white",1)
+makeCircle(3,47,.1,"white",1)
+
 }
 
 
@@ -32,22 +271,22 @@ function createThirdScene() {
 // This function is called whenever you press the "Go!" button.
 function createRandomScene() {
     // Generate a random number between 0 and 1, and store it in a variable.
-    
+    var myNumber = Math.random()
     // If the number is less than 0.33, call the function to create your first scene.
-    
-    
+    if(myNumber<0.33){
+        createFirstScene()
+    }else{
+        if(myNumber<0.67){
+            createSecondScene()
+        }else{
+            createThirdScene()
+        }
+    }
     
     // Else, if the number is less than 0.67, call the function to create your second scene.
-    
-    
-    
+  
     // Else, call the function to create your third scene.
-    
-    
-    
-}
-
-
+        
 
 // DO NOT EDIT ANYTHING BELOW THIS LINE!
 // These are the functions you should call to
@@ -162,4 +401,5 @@ function makeImage(url, x, y, width, height, opacity) {
   var canvas = document.getElementById("canvas")
   canvas.appendChild(image)
   return image
+}
 }
